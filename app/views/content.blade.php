e2cd746c900b63b2b4b8224c71fbbed8b1f0a2ae
Gabriel Moreno Lopez <inggml@gmail.com>
2014-09-23 22:28:23 -0500

    Add markup and css in principal page

--- app/views/content.blade.php
+++ app/views/content.blade.php
@@ -1,49 +1,155 @@
 @extends('template')
 
 @section('content')
-    <!-- Intro Section -->
-    <section id="intro" class="intro-section">
+    <!-- Header Section -->
+    <header>
         <div class="container">
             <div class="row">
-                <div class="col-lg-12">
-                    <h1>Scrolling Nav</h1>
-                    <p><strong>Usage Instructions:</strong> Make sure to include the <code>scrolling-nav.js</code>, <code>jquery.easing.min.js</code>, and <code>scrolling-nav.css</code> files. To make a link smooth scroll to another section on the page, give the link the <code>.page-scroll</code> class and set the link target to a corresponding ID on the page.</p>
-                    <a class="btn btn-default page-scroll" href="#about">Click Me to Scroll Down!</a>
+                <div class="col-lg-6 functions">
+                    <h3><span class="glyphicon glyphicon-bullhorn"></span> Notificaciones</h3>
+                    <p>ipsum dolor sit amet, consectetur adipisicing elit. Laborum porro dolor atque quidem, debitis temporibus, illum nihil.</p>
+                    <h3><span class="glyphicon glyphicon-transfer"></span> Consultas</h3>
+                    <p>ipsum dolor sit amet, consectetur adipisicing elit. Laborum porro dolor atque quidem, debitis temporibus, illum nihil.</p>
+                    <h3><span class="glyphicon glyphicon-thumbs-up"></span> Comparte</h3>
+                    <p>ipsum dolor sit amet, consectetur adipisicing elit. Laborum porro dolor atque quidem, debitis temporibus, illum nihil.</p>
+                    <h3><span class="glyphicon glyphicon-phone"></span> Accesa desde tu Móvil</h3>
+                    <p>ipsum dolor sit amet, consectetur adipisicing elit. Laborum porro dolor atque quidem, debitis temporibus, illum nihil.</p>
+                </div>
+                <div class="col-lg-6">
+                    <img alt="" src="images/travel.png" class="img-responsive diagram">
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- Intro Section -->
+    <section id="quees">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>¿Qué es?</h2>
+                    <hr>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-4">
+                    <span class="glyphicon glyphicon-info-sign icons-info"></span>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo ipsum obcaecati esse, nesciunt pariatur, nulla ipsa rerum possimus officiis, laboriosam voluptatibus consequatur. At beatae, mollitia eaque cum consequatur necessitatibus dolore.</p>
+                </div>
+                <div class="col-sm-4">
+                    <span class="glyphicon glyphicon-user icons-info"></span>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus excepturi ipsa, error magni mollitia, hic quaerat saepe sunt, molestiae nemo illo. Nobis animi quia voluptas reiciendis assumenda fugiat ab sit.</p>
+                </div>
+                <div class="col-sm-4">
+                    <span class="glyphicon glyphicon-stats icons-info"></span>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam fuga ipsa ducimus rerum quibusdam aliquid culpa enim illum recusandae, quis architecto modi aspernatur officia nisi inventore necessitatibus, soluta maiores asperiores.</p>
                 </div>
             </div>
         </div>
     </section>
 
     <!-- About Section -->
-    <section id="about" class="about-section">
+    <section id="comofunciona">
         <div class="container">
             <div class="row">
-                <div class="col-lg-12">
-                    <h1>About Section</h1>
+                <div class="col-lg-12 text-center">
+                    <h2>¿Cómo funciona?</h2>
+                    <hr>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-4">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo ipsum obcaecati esse, nesciunt pariatur, nulla ipsa rerum possimus officiis, laboriosam voluptatibus consequatur. At beatae, mollitia eaque cum consequatur necessitatibus dolore.</p>
+                </div>
+                <div class="col-sm-4">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus excepturi ipsa, error magni mollitia, hic quaerat saepe sunt, molestiae nemo illo. Nobis animi quia voluptas reiciendis assumenda fugiat ab sit.
+                </div>
+                <div class="col-sm-4">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam fuga ipsa ducimus rerum quibusdam aliquid culpa enim illum recusandae, quis architecto modi aspernatur officia nisi inventore necessitatibus, soluta maiores asperiores.
                 </div>
             </div>
         </div>
     </section>
 
-    <!-- Services Section -->
-    <section id="services" class="services-section">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <h1>Services Section</h1>
-                </div>
-            </div>
-        </div>
-    </section>
 
     <!-- Contact Section -->
-    <section id="contact" class="contact-section">
+    <section id="contactanos">
         <div class="container">
             <div class="row">
-                <div class="col-lg-12">
-                    <h1>Contact Section</h1>
+                <div class="col-lg-12 text-center">
+                    <h2>Contáctanos</h2>
+                    <hr class="star-primary">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2">
+                    <form novalidate="" id="contactForm" name="sentMessage">
+                        <div class="row control-group">
+                            <div class="form-group col-xs-12 floating-label-form-group controls">
+                                <label>Nombre</label>
+                                <input type="text" data-validation-required-message="Please enter your name." required="" id="name" placeholder="Nombre" class="form-control">
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <div class="row control-group">
+                            <div class="form-group col-xs-12 floating-label-form-group controls">
+                                <label>Correo Electrónico</label>
+                                <input type="email" data-validation-required-message="Please enter your email address." required="" id="email" placeholder="Correo Electrónico" class="form-control" aria-invalid="false">
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <div class="row control-group">
+                            <div class="form-group col-xs-12 floating-label-form-group controls">
+                                <label>Número telefónico</label>
+                                <input type="tel" data-validation-required-message="Please enter your phone number." required="" id="phone" placeholder="Número telefónico" class="form-control">
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <div class="row control-group">
+                            <div class="form-group col-xs-12 floating-label-form-group controls">
+                                <label>Mensaje</label>
+                                <textarea data-validation-required-message="Please enter a message." required="" id="message" placeholder="Mensaje" class="form-control" rows="5"></textarea>
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <br>
+                        <div id="success"></div>
+                        <div class="row">
+                            <div class="form-group col-xs-12">
+                                <button class="btn btn-success btn-default" type="submit">Enviar Comentario</button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
     </section>
+    <footer class="text-center">
+            <div class="footer-above">
+                <div class="container">
+                    <div class="row">
+                        <div class="footer-col col-md-4">
+                            <h3>Acerca de</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut nihil laudantium illum culpa rem dignissimos eos ratione iste libero labore atque, illo nulla quisquam qui aperiam minima tempora numquam error.</p>
+                        </div>
+                        <div class="footer-col col-md-4">
+                            <h3>Servidores Públicos</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem earum quo optio quibusdam, odit, tempore debitis cum, suscipit inventore non veniam iusto? Magnam dolore molestiae optio, vel atque at iure!</p>
+                        </div>
+                        <div class="footer-col col-md-4">
+                            <h3>Destinos principales</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni id iusto eligendi quidem distinctio mollitia minus maiores, eaque temporibus, est, esse quae! Ab excepturi rerum, obcaecati! Maxime corporis, rerum labore.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-below">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            Todos los Derechos Reservados &copy; 2014
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>    
 @stop
--- app/views/template.blade.php
+++ app/views/template.blade.php
@@ -1,89 +1,84 @@
 <!DOCTYPE html>
-<html lang="en">
-
-<head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>
-        {{{ isset($title) ? 'Viajes Transparentes - '.$title : 'Viajes Transparentes' }}}
-    </title>
-
-    <!-- Bootstrap Core CSS -->
-    <link href="{{ URL::asset('css/bootstrap.min.css') }}" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="{{ URL::asset('css/scrolling-nav.css') }}" rel="stylesheet">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-    <!-- jQuery Version 1.11.0 -->
-    <script src="{{ URL::asset('js/jquery-1.11.0.js') }}"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="{{ URL::asset('js/bootstrap.min.js') }}"></script>
-
-    <!-- Scrolling Nav JavaScript -->
-    <script src="{{ URL::asset('js/jquery.easing.min.js') }}"></script>
-    <script src="{{ URL::asset('js/scrolling-nav.js') }}"></script>
-
-
-</head>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <title>
+            {{{ isset($title) ? 'Viajes Transparentes - '.$title : 'Viajes Transparentes' }}}
+        </title>
+        <link href='http://fonts.googleapis.com/css?family=Noto+Sans:400,700' rel='stylesheet' type='text/css'>
+        <!-- Bootstrap Core CSS -->
+        <link href="{{ URL::asset('css/bootstrap.min.css') }}" rel="stylesheet">
+        <!-- Custom CSS -->
+        <link href="{{ URL::asset('css/estilos.css') }}" rel="stylesheet">
+    </head>
 
 <!-- The #page-top ID is part of the scrolling feature - the data-spy and data-target are part of the built-in Bootstrap scrollspy function -->
 
-<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+    <body id="page-top" class="index">
+        <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <nav class="navbar navbar-default navbar-fixed-top">
+                <div class="container">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="navbar-header page-scroll">
+                        <button data-target="#bs-example-navbar-collapse-1" data-toggle="collapse" class="navbar-toggle" type="button">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a href="#page-top" class="navbar-brand">
+                            <span class="glyphicon glyphicon-globe icon-logo"></span> Viajes Transparentes
+                        </a>
 
-    <!-- Navigation -->
-    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-        <div class="container">
-            <div class="navbar-header page-scroll">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand page-scroll" href="#page-top">Viajes Transparentes</a>
-            </div>
+                    </div>
 
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse navbar-ex1-collapse">
-                <ul class="nav navbar-nav">
-                    <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
-                    <li class="hidden">
-                        <a class="page-scroll" href="#page-top"></a>
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="#about">Acerca</a>
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="#data">Datos</a>
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="#contact">Contacto</a>
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="#login">Login</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div id="bs-example-navbar-collapse-1" class="collapse navbar-collapse">
+                        <ul class="nav navbar-nav navbar-right">
+                            <li class="page-scroll">
+                                <a href="#quees">¿Qué es?</a>
+                            </li>
+                            <li class="page-scroll">
+                                <a href="#comofunciona">¿Cómo funciona?</a>
+                            </li>
+                            <li class="page-scroll">
+                                <a href="#contactanos">Contáctanos</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         </div>
-        <!-- /.container -->
-    </nav>
 
-    @yield('content')
+        @yield('content')
 
-
-</body>
+        <!-- jQuery Version 1.11.0 -->
+        <script src="{{ URL::asset('js/jquery-1.11.0.js') }}"></script>
+        <!-- Bootstrap Core JavaScript -->
+        <script src="{{ URL::asset('js/bootstrap.min.js') }}"></script>
+        <!-- Scrolling Nav JavaScript -->
+        <script src="{{ URL::asset('js/jquery.easing.min.js') }}"></script>
+        <script src="{{ URL::asset('js/scrolling-nav.js') }}"></script>
+        <script type="text/javascript">
+            // jQuery for page scrolling feature - requires jQuery Easing plugin
+            $(function() {
+                $('.page-scroll a').bind('click', function(event) {
+                    var $anchor = $(this);
+                    $('html, body').stop().animate({
+                        scrollTop: $($anchor.attr('href')).offset().top
+                    }, 1500, 'easeInOutExpo');
+                    event.preventDefault();
+                });
+                // Highlight the top nav as scrolling occurs
+                $('body').scrollspy({
+                    target: '.navbar-fixed-top'
+                })            
+            });
+        </script>        
+    </body>
 
 </html>
