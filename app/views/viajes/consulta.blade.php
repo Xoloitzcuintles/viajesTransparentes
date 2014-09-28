43f4ca8598a3d9a4e1d1840638271beb4223768e
Gabriel Moreno Lopez <inggml@gmail.com>
2014-09-28 16:52:21 -0500

    Add filter markup on consulta view

--- app/views/viajes/consulta.blade.php
+++ app/views/viajes/consulta.blade.php
@@ -1,27 +1,37 @@
 @section('consulta')
                 <script src="{{ URL::asset('js/lib/viajes/consulta.js') }}"></script>
-                <div class="col-sm-3">
-                    <h1>Búsqueda</h1>
-                    <p>
-                {{ Form::open(array('url' => '#', 'id' => 'consulta_viajes')) }}
-                
-                {{ Form::label('servidor', 'Servidor') }}
- 
-                {{ Form::text('servidor', Input::old('servidor'), array('id' => 'servidor_id')) }}
-<!-- <br />
-                {{ Form::label('username', 'Nick') }}
-                {{ Form::text('username', Input::old('username')) }}
- <br />
-                {{ Form::label('password', 'Password') }}
-                {{ Form::password('password') }}
- <br />
-                {{ Form::label('password_confirmation', 'Confirm password') }}
-                {{ Form::password('password_confirmation') }}
-                !-->
-                <br />
-                {{ Form::submit('Buscar', array('class' => 'button expand round btn btn-default', 'id' => 'buscar_viajes_button')) }}
- 
-                {{ Form::close() }}
-                    </p>
-                </div>
+                    <div class="col-sm-3">
+                        <h3>Buscador</h3>
+                            {{ Form::open(array('url' => '#', 'id' => 'consulta_viajes')) }}
+                            
+                            <div class='input-group date' id='datetimepickend'>
+                                <span class="input-group-addon">
+                                    <span class="glyphicon glyphicon-search"></span>
+                                </span>
+                                {{ Form::text('servidor', Input::old('servidor'), array('id' => 'servidor_id','placeholder'=>'Buscar...','class'=>'form-control')) }}
+                            </div>
+                            <div class='input-group date' id='datetimepickbegin'>
+                                <input type='text' class="form-control" placeholder="Fecha inicio"/>
+                                <span class="input-group-addon">
+                                    <span class="glyphicon glyphicon-calendar"></span>
+                                </span>
+                            </div>
+                            <div class='input-group date' id='datetimepickend'>
+                                <input type='text' class="form-control" placeholder="Fecha final"/>
+                                <span class="input-group-addon">
+                                    <span class="glyphicon glyphicon-calendar"></span>
+                                </span>
+                            </div>                            
+                            <div class="input-group">
+                                <select class="form-control">
+                                    <option selected="selected">Buscar por...</option>
+                                    <option>Área de adscripción</option>
+                                    <option>Tema</option>
+                                    <option>Perfil servidor público</option>
+                                    <option>Ubicación geográfica</option>
+                                </select>
+                            </div>
+                            {{ Form::submit('Realizar búsqueda', array('class' => 'button expand round btn btn-default', 'id' => 'buscar_viajes_button')) }}
+                        {{ Form::close() }}
+                    </div>
 @stop
