66141b281de569751448c321b19c6d1b863c8e6c
Adro Rocker <adro.skate@gmail.com>
2014-09-29 23:17:32 -0500

    Servidor seeder

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -24,6 +24,7 @@ class DatabaseSeeder extends Seeder {
 		$this->call('InstGeneraTableSeeder');
 		$this->call('CompaniaTransporteTableSeeder');
 		$this->call('TemaTableSeeder');
+		$this->call('ServidoresTableSeeder');
 	}
 
 }
