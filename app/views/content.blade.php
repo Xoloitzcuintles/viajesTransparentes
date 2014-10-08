2768ea69a06214612fa89e1082f8a93762420a39
gabotrons <gabriel.moreno@linio.com>
2014-10-08 16:10:30 -0500

    Change width columns

--- app/views/content.blade.php
+++ app/views/content.blade.php
@@ -193,13 +193,13 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col-sm-8">
+                <div class="col-sm-6">
                     <!-- 16:9 aspect ratio -->
                     <div class="embed-responsive embed-responsive-16by9">
                       <iframe class="embed-responsive-item" src="//www.youtube.com/embed/0lqWkIdayIA?rel=0&vq=hd1080"></iframe>
                     </div>
                 </div>
-                <div class="col-sm-4">
+                <div class="col-sm-6">
                     <p>En este video podrás encontrar una breve explicación sobre el desarrollo de nuestro sistema, así como las funcionalidades principales y características más importantes.</p>
                     <div class="btn-group btn-group-justified social">
                         <a class="btn btn-default fb" role="button">Facebook</a>
